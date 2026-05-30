87aefb5f661b86954756617e7d0f0176565ebfb6
abir2907 <abirchodha1@gmail.com>
2026-05-30 16:19:05 +0530

    Use postConstruct and preDelete on beans

--- module1introduction/src/main/java/com/spring/abir/module1introduction/PaymentService.java
+++ module1introduction/src/main/java/com/spring/abir/module1introduction/PaymentService.java
@@ -1,10 +1,22 @@
 package com.spring.abir.module1introduction;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentService {
     public void pay() {
         System.out.println("Paying...");
+    }
+
+    @PostConstruct
+    public void afterInit() {
+        System.out.println("Before Payment");
+    }
+
+    @PreDestroy
+    public void beforeDel() {
+        System.out.println("After Payment");
     }
 }
