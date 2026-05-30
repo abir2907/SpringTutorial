00cadac690de833741da40be0a53f69ed564acb8
abir2907 <abirchodha1@gmail.com>
2026-05-30 16:29:06 +0530

    Edit bean scope to prototype, now diff objs created
    Now each time a different obj will be created (as we can see from the different hashCode) and now the preDelete print statement of 'After Payment' won't execute because with a Prototype scope, Spring's relationship with the bean is strictly 'fire and forget'. Instead of Spring destroying the bean, it is entirely up to the standard Java Garbage Collector to eventually sweep that object out of memory once your code stops referencing it.

--- module1introduction/src/main/java/com/spring/abir/module1introduction/AppConfig.java
+++ module1introduction/src/main/java/com/spring/abir/module1introduction/AppConfig.java
@@ -2,12 +2,14 @@ package com.spring.abir.module1introduction;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Configuration
 public class AppConfig {
 
     @Bean
+    @Scope("prototype")
     PaymentService paymentService() {
         // more custom logic
         return new PaymentService();
