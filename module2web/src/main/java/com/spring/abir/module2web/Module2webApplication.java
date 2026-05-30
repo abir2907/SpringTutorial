8c4e0342e4c835090de66b3261aa0da2da2b56a6
abir2907 <abirchodha1@gmail.com>
2026-05-31 02:12:17 +0530

    Create new Srping Web Application with folder structure

---
+++ module2web/src/main/java/com/spring/abir/module2web/Module2webApplication.java
@@ -0,0 +1,14 @@
+package com.spring.abir.module2web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module2webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module2webApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ module2web/src/test/java/com/spring/abir/module2web/Module2webApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.abir.module2web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module2webApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
