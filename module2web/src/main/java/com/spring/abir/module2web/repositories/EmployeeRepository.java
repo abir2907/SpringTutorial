4999384dd7c07db48c49e1d973ed6fc5b23440d7
abir2907 <abirchodha1@gmail.com>
2026-05-31 16:37:04 +0530

    Add a custom query that JPA handles for us
    Because you put this method inside an interface that extends JpaRepository, Spring Boot takes over completely. When you start your application, Spring reads the name of your method, parses the words, and automatically writes the SQL query for you behind the scenes. The Action (findBy): Spring reads findBy (or readBy, queryBy, getBy) and instantly knows you want to perform a SELECT * FROM employees query.

--- module2web/src/main/java/com/spring/abir/module2web/repositories/EmployeeRepository.java
+++ module2web/src/main/java/com/spring/abir/module2web/repositories/EmployeeRepository.java
@@ -4,5 +4,5 @@ import com.spring.abir.module2web.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-
+    List<EmployeeEntity> findByName(String name);
 }
