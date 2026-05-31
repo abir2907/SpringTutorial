81d1b5b6c3ee43999207035b84aea66db7e2d752
abir2907 <abirchodha1@gmail.com>
2026-05-31 16:25:17 +0530

    Create the Employee Entity
    @GeneratedValue(strategy = GenerationType.AUTO): This tells Hibernate and the database, Do not make me calculate or provide the ID manually. When I save a new employee, please automatically generate the next available ID for me. Every standard variable we write inside an @Entity class is automatically converted into a column in your database table.

--- module2web/src/main/java/com/spring/abir/module2web/entities/EmployeeEntity.java
+++ module2web/src/main/java/com/spring/abir/module2web/entities/EmployeeEntity.java
@@ -1,9 +1,18 @@
 package com.spring.abir.module2web.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "employees")
 public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfJoining;
+    private Boolean isActive;
 }
