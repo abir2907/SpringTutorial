ad1b8944313bd1f7500cc0cbe6f977c8d912ad61
abir2907 <abirchodha1@gmail.com>
2026-05-31 16:14:11 +0530

    Initialise an Employee Entity
    The @Enitity annotaion tells the data-jpa (ie Hibernate) to convert this java class into an actual MySQL Database

---
+++ module2web/src/main/java/com/spring/abir/module2web/entities/EmployeeEntity.java
@@ -0,0 +1,9 @@
+package com.spring.abir.module2web.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class EmployeeEntity {
+}
