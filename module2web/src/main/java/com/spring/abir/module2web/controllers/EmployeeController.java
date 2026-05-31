cb47c149a58ea03c6701f3e1869e41d05ff00e1b
abir2907 <abirchodha1@gmail.com>
2026-05-31 14:24:08 +0530

    Extract base path to class-level RequestMapping in EmployeeController

--- module2web/src/main/java/com/spring/abir/module2web/controllers/EmployeeController.java
+++ module2web/src/main/java/com/spring/abir/module2web/controllers/EmployeeController.java
@@ -1,23 +1,21 @@
 package com.spring.abir.module2web.controllers;
 
 import com.spring.abir.module2web.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
 
-    @GetMapping(path = "/employees/{employeeId}")
+    @GetMapping(path = "/{employeeId}")
     public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
         return new EmployeeDTO(employeeId, "Abir", "abir@gmail.com", 23, LocalDate.of(2026, 3, 1), true);
     }
 
-    @GetMapping(path = "/employees")
+    @GetMapping()
     public String getAllEmployees(@RequestParam(required = false) Integer age,
                                   @RequestParam(required = false) String sortBy) {
         return "Hi age " + age + ", sort by: " + sortBy;
