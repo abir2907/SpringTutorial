dc88e8d81ec16a01457d8cac94f702e4fd351f22
abir2907 <abirchodha1@gmail.com>
2026-05-31 14:02:09 +0530

    Create getEmployeeByID func that returns EmployeeDTO

--- module2web/src/main/java/com/spring/abir/module2web/controllers/EmployeeController.java
+++ module2web/src/main/java/com/spring/abir/module2web/controllers/EmployeeController.java
@@ -1,13 +1,18 @@
 package com.spring.abir.module2web.controllers;
 
+import com.spring.abir.module2web.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class EmployeeController {
 
-    @GetMapping(path = "/getSecretMessage")
-    public String getSecretMessage() {
-        return "Secret Msg: ABC";
+    @GetMapping(path = "/employees/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
+        return new EmployeeDTO(employeeId, "Abir", "abir@gmail.com", 23, LocalDate.of(2026, 3, 1), true);
     }
+
 }
