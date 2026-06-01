9388270328ec5a5eee6a96e10337025db2f09442
abir2907 <abirchodha1@gmail.com>
2026-06-02 00:44:53 +0530

    Initialise new JPA project

---
+++ module3jpa/src/main/java/com/spring/abir/module3jpa/Module3jpaApplication.java
@@ -0,0 +1,14 @@
+package com.spring.abir.module3jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module3jpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module3jpaApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ module3jpa/src/test/java/com/spring/abir/module3jpa/Module3jpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.abir.module3jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module3jpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
