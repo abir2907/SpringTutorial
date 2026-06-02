108d0a5322079a1eddf7ca149cbae39f0d3ba8f1
abir2907 <abirchodha1@gmail.com>
2026-06-02 17:14:25 +0530

    Define queries using method name

--- module3jpa/src/main/java/com/spring/abir/module3jpa/repositories/ProductRepository.java
+++ module3jpa/src/main/java/com/spring/abir/module3jpa/repositories/ProductRepository.java
@@ -4,10 +4,29 @@ import com.spring.abir.module3jpa.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     List<ProductEntity> findByTitle(String title);
+
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime after);
+
+    List<ProductEntity> findByQuantityAndPrice(Integer quantity, BigDecimal price);
+
+    List<ProductEntity> findByQuantityGreaterThanAndPriceLessThan(Integer quantity, BigDecimal price);
+
+    List<ProductEntity> findByQuantityGreaterThanOrPriceLessThan(Integer quantity, BigDecimal price);
+
+    List<ProductEntity> findByTitleLike(String title);
+
+    List<ProductEntity> findByTitleContaining(String title);
+
+    List<ProductEntity> findByTitleContainingIgnoreCase(String title);
+
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
 }
--- module3jpa/src/test/java/com/spring/abir/module3jpa/Module3jpaApplicationTests.java
+++ module3jpa/src/test/java/com/spring/abir/module3jpa/Module3jpaApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class Module3jpaApplicationTests {
@@ -34,8 +35,28 @@ class Module3jpaApplicationTests {
 
 	@Test
 	void getRepository() {
-        List<ProductEntity> productEntityList = productRepository.findByTitle("Pepsi");
+		/*
+        List<ProductEntity> productEntityList = productRepository.findByCreatedAtAfter(
+				LocalDateTime.of(2024,1,1,0,0,0,0)
+		);
+		*/
+
+		// List<ProductEntity> productEntityList = productRepository.findByQuantityAndPrice(4, BigDecimal.valueOf(12.4));
+		// List<ProductEntity> productEntityList = productRepository.findByQuantityGreaterThanAndPriceLessThan(3, BigDecimal.valueOf(14));
+		// List<ProductEntity> productEntityList = productRepository.findByQuantityGreaterThanOrPriceLessThan(3, BigDecimal.valueOf(14));
+		// List<ProductEntity> productEntityList = productRepository.findByTitleLike("p%");
+		// List<ProductEntity> productEntityList = productRepository.findByTitleContaining("epsi");
+		 List<ProductEntity> productEntityList = productRepository.findByTitleContainingIgnoreCase("EPSI");
+
 		System.out.println(productEntityList);
+	}
+
+	@Test
+	void getSingleFromRepository() {
+		// We have defined a constraint on title and price, therefore we can be sure that each pair of title & price will be unique
+		// Hence we can get a single entity instead of a List
+		Optional<ProductEntity> productEntity = productRepository.findByTitleAndPrice("Mazza", BigDecimal.valueOf(16.4));
+		productEntity.ifPresent(System.out::println);
 	}
 
 }
