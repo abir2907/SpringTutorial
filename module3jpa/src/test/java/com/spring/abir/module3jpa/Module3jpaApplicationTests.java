30439a2347deda3f6c177e825c80b5cf4519e79e
abir2907 <abirchodha1@gmail.com>
2026-06-02 13:37:29 +0530

    Create a test to check inserting data

--- module3jpa/src/test/java/com/spring/abir/module3jpa/Module3jpaApplicationTests.java
+++ module3jpa/src/test/java/com/spring/abir/module3jpa/Module3jpaApplicationTests.java
@@ -1,9 +1,12 @@
 package com.spring.abir.module3jpa;
 
+import com.spring.abir.module3jpa.entities.ProductEntity;
 import com.spring.abir.module3jpa.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
 
 @SpringBootTest
 class Module3jpaApplicationTests {
@@ -13,6 +16,19 @@ class Module3jpaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testRepository() {
+		ProductEntity productEntity = ProductEntity.builder()
+				.sku("nestle234")
+				.title("Nestle Chocolate")
+				.price(BigDecimal.valueOf(123.45))
+				.quantity(12)
+				.build();
+
+		ProductEntity savedProductEntity = productRepository.save(productEntity);
+		System.out.println(savedProductEntity);
 	}
 
 }
