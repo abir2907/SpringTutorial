b476cf2fed13ed3f9709c8c65c8aea4b49c51355
abir2907 <abirchodha1@gmail.com>
2026-06-03 20:57:23 +0530

    Initialise hospital management project

---
+++ module3hospital/src/main/java/com/spring/abir/module3hospital/Module3hospitalApplication.java
@@ -0,0 +1,15 @@
+package com.spring.abir.module3hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module3hospitalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Module3hospitalApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ module3hospital/src/test/java/com/spring/abir/module3hospital/Module3hospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.abir.module3hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module3hospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
