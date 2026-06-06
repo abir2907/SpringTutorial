bf169112506bd11d1ba0f6c9d06eb79aa3761e30
abir2907 <abirchodha1@gmail.com>
2026-06-06 21:54:37 +0530

    Create appointment entity

---
+++ module3hospital/src/main/java/com/spring/abir/module3hospital/entity/Appointment.java
@@ -0,0 +1,26 @@
+package com.spring.abir.module3hospital.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentTime;
+
+    @Column(length = 500)
+    private String reason;
+
+}
