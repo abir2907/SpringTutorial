cfd69181b9a04ae63bc7c29ff1c936225548673f
abir2907 <abirchodha1@gmail.com>
2026-06-08 18:25:10 +0530

    Implement get current auditor

--- module4/src/main/java/com/spring/abir/module4/config/AppConfig.java
+++ module4/src/main/java/com/spring/abir/module4/config/AppConfig.java
@@ -1,17 +1,24 @@
 package com.spring.abir.module4.config;
 
+import com.spring.abir.module4.auth.AuditorAwareImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "getAuditorAwareImpl")
 public class AppConfig {
 
     @Bean
     ModelMapper getModelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    AuditorAware<String> getAuditorAwareImpl() {
+        return new AuditorAwareImpl();
     }
 
 }
