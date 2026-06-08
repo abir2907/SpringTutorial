6a235e7faed34c924eaa370184e9a31ad577c927
abir2907 <abirchodha1@gmail.com>
2026-06-08 15:35:06 +0530

    Implement create and get all posts

--- module4/src/main/java/com/spring/abir/module4/config/AppConfig.java
+++ module4/src/main/java/com/spring/abir/module4/config/AppConfig.java
@@ -1,4 +1,15 @@
 package com.spring.abir.module4.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+
 }
--- module4/src/main/java/com/spring/abir/module4/services/PostServiceImpl.java
+++ module4/src/main/java/com/spring/abir/module4/services/PostServiceImpl.java
@@ -1,25 +1,34 @@
 package com.spring.abir.module4.services;
 
 import com.spring.abir.module4.dto.PostDTO;
+import com.spring.abir.module4.entities.PostEntity;
 import com.spring.abir.module4.repositories.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService{
 
     private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public List<PostDTO> getAllPosts() {
-        return List.of();
+        return postRepository
+                .findAll()
+                .stream()
+                .map(postEntity -> modelMapper.map(postEntity, PostDTO.class))
+                .collect(Collectors.toList());
     }
 
     @Override
     public PostDTO createNewPost(PostDTO inputPost) {
-        return null;
+        PostEntity postEntity = modelMapper.map(inputPost, PostEntity.class);
+        return modelMapper.map(postRepository.save(postEntity), PostDTO.class);
     }
 }
