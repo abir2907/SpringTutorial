1e393a65ab838f2d6dc0e0e1f7d9d9a57cbcd792
abir2907 <abirchodha1@gmail.com>
2026-06-08 18:16:40 +0530

    Add auditing by implementing super class

--- module4/src/main/java/com/spring/abir/module4/config/AppConfig.java
+++ module4/src/main/java/com/spring/abir/module4/config/AppConfig.java
@@ -3,8 +3,10 @@ package com.spring.abir.module4.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class AppConfig {
 
     @Bean
