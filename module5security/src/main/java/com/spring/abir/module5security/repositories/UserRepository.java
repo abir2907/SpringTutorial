3226daa4c9a40897353938688628c9ec3945253a
abir2907 <abirchodha1@gmail.com>
2026-06-15 18:22:29 +0530

    Implement user details and user details service

---
+++ module5security/src/main/java/com/spring/abir/module5security/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.spring.abir.module5security.repositories;
+
+import com.spring.abir.module5security.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+}
